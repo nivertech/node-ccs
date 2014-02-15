8cd3d8cb44bb7272144cd2898d4de1720bce9a5c
Seshachala Malisetti <seshachalam@outlook.com>
2014-02-16 03:15:31 +0530

    used https://gist.github.com/jamesvnz/6756208

--- nodegear.js
+++ nodegear.js
@@ -1,6 +1,6 @@
 //var gearmanode = require('gearmanode');
 //var redis = require("redis");
-var xmpp = require('node-xmpp-client');
+var xmpp = require('node-xmpp');
 
 var gearJob;
 var redisSubChan = 'test_channel';
@@ -11,14 +11,19 @@ var xmppClient;
 var gearClient;
 
 //gearClient = gearmanode.client();
-
-xmppClient = new xmpp.Client({
-    jid: '1026645507924',
+var options = {
+    type: 'client',
+    jid: '1026645507924@gcm.googleapis.com',
     password: 'AIzaSyDxf8TTgcl4KLHv-4HsEp8nUmnjENMTt6A',
     port: 5235,
     host: 'gcm.googleapis.com',
-    saslmech: 'PLAIN'
-});
+    legacySSL: true,
+    preferredSaslMechanism: 'PLAIN'
+};
+
+console.log('creating xmpp app');
+
+xmppClient = new xmpp.Client(options);
 
 //xmppClient.connection.socket.setTimeout(0)
 //xmppClient.connection.socket.setKeepAlive(true, 10000)
@@ -27,32 +32,75 @@ xmppClient = new xmpp.Client({
 //redisClient.subscribe(redisSubChan);
 
 //redisClient.on("message", function(channel, message) {
-    //send the messages to google ccs server via xmpp
+//send the messages to google ccs server via xmpp
 //});
 
 //receive messages from ccs and give it to PHP workers
 /*
-gearJob = gearClient.submitJob('reverse', message, {background: true});
-
-gearJob.on('workData', function(data) {
-    console.log('WORK_DATA >>> ' + data);
+ gearJob = gearClient.submitJob('reverse', message, {background: true});
+ 
+ gearJob.on('workData', function(data) {
+ console.log('WORK_DATA >>> ' + data);
+ });
+ 
+ gearJob.on('complete', function() {
+ console.log('RESULT >>> ' + gearJob.response);
+ gearClient.close();
+ });
+ */
+xmppClient.on('online', function() {
+    console.log("online");
 });
-
-gearJob.on('complete', function() {
-    console.log('RESULT >>> ' + gearJob.response);
-    gearClient.close();
-});
-*/
 
 xmppClient.on('connection', function() {
     console.log('online')
 });
 
-xmppClient.on('stanza', function(stanza) {
-    console.log('Incoming stanza: ', stanza.toString())
-});
+xmppClient.on('stanza',
+        function(stanza) {
+            if (stanza.is('message') &&
+// Best to ignore an error
+                    stanza.attrs.type !== 'error') {
+
+                console.log("Message received");
+//Message format as per here: https://developer.android.com/google/gcm/ccs.html#upstream
+                var messageData = JSON.parse(stanza.getChildText("gcm"));
+
+                if (messageData && messageData.message_type != "ack" && messageData.message_type != "nack") {
+
+                    var ackMsg = new xmpp.Element('message').c('gcm', {xmlns: 'google:mobile:data'}).t(JSON.stringify({
+                        "to": messageData.from,
+                        "message_id": messageData.message_id,
+                        "message_type": "ack"
+                    }));
+//send back the ack.
+                    cl.send(ackMsg);
+                    console.log("Sent ack");
+
+//Now do something useful here with the message
+//e.g. awesomefunction(messageData);
+//but let's just log it.
+                    console.log(messageData);
+
+                } else {
+//Need to do something more here for a nack.
+                    console.log("message was an ack or nack...discarding");
+                }
+
+            } else {
+                console.log("error");
+                console.log(stanza)
+            }
+
+        });
 
 xmppClient.on('authenticate', function(opts, cb) {
     console.log('AUTH' + opts.jid + ' -> ' + opts.password)
     cb(null);
+});
+
+xmppClient.on('error', function(e) {
+    console.log("Error occured:");
+    console.error(e);
+    console.error(e.children);
 });
